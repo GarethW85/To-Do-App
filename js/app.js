33c3c29b8b34e8ebad321d38a6db9aa3ba0a4f5b
GarethW85 <tidylad@hotmail.com>
2022-06-21 19:04:38 +0200

    updates

--- js/app.js
+++ js/app.js
@@ -14,7 +14,6 @@ for (i = 0; i < mylist.length; i++) {
 
 
 
-
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
@@ -24,8 +23,6 @@ for (i = 0; i < close.length; i++) {
     div.style.display = "none";
   }
 }
-
-
 
 
 // Add a "checked" symbol when clicking on a list item
@@ -48,10 +45,9 @@ function myElement() {
 
   li.appendChild(t);
   if (inputValue === '') {
-    // alert("Your list can't be blank!"); with a hidden feature after animation
     document.getElementById("errorMsg").style.display = 'block';
     setTimeout(function(){
-      errorMsg.style.display = 'none'; }, 2500); 
+      errorMsg.style.display = 'none'; }, 2000); 
   } else {
     document.getElementById("myUL").appendChild(li);
   }
@@ -59,8 +55,7 @@ function myElement() {
 
   var span = document.createElement("SPAN");
   // var txt = document.createTextNode("\u00D7");
-  var txt = document.createTextNode("\u2421");
-  
+  var txt = document.createTextNode("\u2717");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
@@ -76,9 +71,7 @@ function myElement() {
 
 
 //Delete all listed items and reload the page
-
 function clearAll(x) {
-
   switch (x) {
     case 'removeList':
       document.getElementById('myUL').style.display='none';
