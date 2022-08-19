9d49f8416885fa45d063fb8433906244f6e698d5
GarethW85 <tidylad@hotmail.com>
2022-08-19 21:19:58 +0200

    Made corrections as per the feedback
    Removed the animate css CDN file from the todo.html file as well as the class name"animate__animated"
    
    Corrected the indentation on the JS file
    
    Made sure that all commits have the correct description
    
    Got rid of white spaces when adding items ... you can no longer as blank items.
    
    Variables have been changed to Let & Const . Var is present but not when inside a function
    
    Made sure there is a break at the within the while loop .
    
    Corrected naming conventions .

--- js/app.js
+++ js/app.js
@@ -2,6 +2,22 @@
 ////// JAVASCRIPT IS MY NEW FAVOURITE LANGUAGE ///////////
 //////////////////////////////////////////////////////////
 
+// Prevent white spaces from been added to the list
+//////////////////////////////////////////////////////////
+document.addEventListener("DOMContentLoaded", function () {
+  const input = document.getElementById("myInput");
+
+  if (!input) return;
+  input.addEventListener("input", (e) => {
+    const value = e.target.value;
+    const hasTrailingSpace = value !== " " && value.charAt(value.length - 1) === " ";
+    const words = value.split(" ").filter((el) => el !== "");
+    let sanitizedValue = words.join(" ");
+
+    if (hasTrailingSpace) sanitizedValue += " ";
+    e.target.value = sanitizedValue;
+  });
+});
 
 // Create a "close" button X
 //////////////////////////////////////////////////////////
@@ -14,57 +30,52 @@ for (i = 0; i < mylist.length; i++) {
   mylist[i].appendChild(span);
 }
 
-
 // Close button to hide the current list item
 ////////////////////////////////////////////////////////
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
-  }
+  };
 }
-
 
 // Create a new list item when clicking on the "Add" button
 ///////////////////////////////////////////////////////////
 function myElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  var animate = document.getElementById('errorMsg');
-  
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("myInput").value;
+  let t = document.createTextNode(inputValue);
+
   // get date from input field
-var new_data = document.getElementById('myInput').value;
-const input = document.getElementById('myInput');
+  let new_data = document.getElementById("myInput").value;
+  const input = document.getElementById("myInput");
 
+  //validation on empty submits
+  if (input.value === "") {
+    swal("Sorry!", "No empty fields accepted!", "error");
+    return false;
+  }
 
-//validation on empty submits 
-if (input.value === ""){
-  // alert ('You cant save empty fields');
-  swal("Sorry!", "No empty fields accepted!", "error");
-  return false;
-}
+  // if storage is empty then
+  if (localStorage.getItem("data") == null) {
+    localStorage.setItem("data", "[]");
+  }
 
-// if storage is empty then 
-if (localStorage.getItem('data') == null ) {
-  localStorage.setItem('data', '[]')
-}
+  //get old data
+  let old_data = JSON.parse(localStorage.getItem("data"));
+  old_data.push(new_data);
 
-//get old data
-var old_data = JSON.parse(localStorage.getItem('data'));
-old_data.push(new_data);
-
-//save new data
-localStorage.setItem('data', JSON.stringify(old_data));
-
+  //save new data
+  localStorage.setItem("data", JSON.stringify(old_data));
 
   li.appendChild(t);
-  if (inputValue === '') {
-    document.getElementById("errorMsg").style.display = 'block';
-    setTimeout(function(){
-      errorMsg.style.display = 'none'; }, 2000); 
+  if (inputValue === "") {
+    document.getElementById("errorMsg").style.display = "block";
+    setTimeout(function () {
+      errorMsg.style.display = "none";
+    }, 2000);
   } else {
     document.getElementById("myUL").appendChild(li);
   }
@@ -72,135 +83,84 @@ localStorage.setItem('data', JSON.stringify(old_data));
 
   //Strikethrough on List items
   /////////////////////////////////////////////////////////
-  li.addEventListener('dblclick', function () {
-    li.style.textDecoration = "line-through"
-  })
+  li.addEventListener("dblclick", function () {
+    li.style.textDecoration = "line-through";
+  });
 
-  li.addEventListener('click', function () {
-    li.style.textDecoration = "none"
-  })
+  li.addEventListener("click", function () {
+    li.style.textDecoration = "none";
+  });
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u2717");
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u2717");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
+    close[i].onclick = function () {
+      let div = this.parentElement;
       div.style.display = "none";
-    }
+    };
   }
 }
-
 
 // Delete all listed items and reload the page
 /////////////////////////////////////////////
 function clearAll(x) {
-  
   switch (x) {
-    case 'removeList':
-      document.getElementById('myUL').style.display='none';
+    case "removeList":
+      document.getElementById("myUL").style.display = "none";
       location.reload();
-      break; 
-    default :
-    console.log ('Cant clear list');   
+      break;
+    default:
+      console.log("Cant clear list");
   }
 }
-
 
 // Sort by alphabetical order on click
 //////////////////////////////////////////////////////////
 function sortList() {
+  let list, i, switching, listitems, shouldSwitch;
+  list = document.getElementById("myUL");
+  switching = true;
 
-  var list, i , switching, listitems, shouldSwitch;
-    list = document.getElementById('myUL');
-    switching = true;
- 
-    while (switching) {
-       switching = false;
-       listitems = list.getElementsByTagName("li");
- 
-       for (i = 0 ; i < (listitems.length-1); i++ ){
-       shouldSwitch = false;
-       if(listitems[i].innerHTML.toLowerCase() > listitems[i+1].innerHTML.toLowerCase()){
-          shouldSwitch = true;
-          break;
-          }
-       }
-       listitems[i].parentNode.insertBefore(listitems[i+1],listitems[i]);
-       switching = true;
+  while (switching) {
+    switching = false;
+    listitems = list.getElementsByTagName("li");
+
+    for (i = 0; i < listitems.length - 1; i++) {
+      shouldSwitch = false;
+      if (
+        listitems[i].innerHTML.toLowerCase() >
+        listitems[i + 1].innerHTML.toLowerCase()
+      ) {
+        shouldSwitch = true;
+        break;
+      }
     }
+    listitems[i].parentNode.insertBefore(listitems[i + 1], listitems[i]);
+    switching = true;
+  }
 }
-
-
-// // Local Storage
-// ///////////////////////////////////////////////////////
-
-// function save() {
-// // get date from input field
-// var new_data = document.getElementById('myInput').value;
-// const input = document.getElementById('myInput');
-
-// //validation on blank fields
-// if (input.value === ""){
-//   alert ('You cant save empty fields');
-//   return false;
-// }
-
-
-// // it storage is empty then 
-// if (localStorage.getItem('data') == null ) {
-//   localStorage.setItem('data', '[]')
-// }
-
-// //get old data
-// var old_data = JSON.parse(localStorage.getItem('data'));
-// old_data.push(new_data);
-
-// //save new data
-// localStorage.setItem('data', JSON.stringify(old_data));
-// }
-
 
 //Edit List Items
 ///////////////////////////////////////////////////////
 function edit() {
-
-  var editMe = document.querySelectorAll("[contenteditable=false]");
-  // document.getElementById("editList").style.visibility = 'hidden';
-  // document.getElementById("completed").style.visibility = 'visible';
-  // var icon = document.getElementById("editList");
-  // el.classList.toggle('fa-check');
-  // icon.classList.toggle('fa-check');
-  // above was attemps with buttons to toggle between them onclick ... aborted that idea 
-  for(var i=0; i<editMe.length; i++)
-    editMe[i].setAttribute("contenteditable", true);
-    
-  //   var keydown = document.getElementById("myUL");
-  //   keydown.addEventListener("keydown", function (e) {
-  //   if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
-  //       validate(e);
-  //    return false;
-  //   }
-  // });
-  // above was working but stopped ... had to find a work around 
-  document.addEventListener('keypress', function (e) {
-    if (e.keyCode === 13 || e.which === 13) {
+  let editMe = document.querySelectorAll("[contenteditable=false]");
+  for (let i = 0; i < editMe.length; i++)
+    document.addEventListener("keypress", function (e) {
+      if (e.keyCode === 13 || e.which === 13) {
         e.preventDefault();
         return false;
-    }    
-  });
-  }
-  
+      }
+    });
+}
+
 //Complete button to click when done with edits
 //////////////////////////////////////////////////////
-  function completed() {
-    var doneEdit = document.querySelectorAll("[contenteditable=true]");
-    for(var i=0; i<doneEdit.length; i++)
-    doneEdit[i].setAttribute("contenteditable", false); 
-  }
-  
-
-  
+function completed() {
+  let doneEdit = document.querySelectorAll("[contenteditable=true]");
+  for (let i = 0; i < doneEdit.length; i++)
+    doneEdit[i].setAttribute("contenteditable", false);
+}
